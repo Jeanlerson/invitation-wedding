7af4e5e41198d8eec815e931e808af0175ea6e6f
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2025-09-17 15:53:02 -0300

    atl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Page() {
   return (
     <div className="flex justify-center min-h-screen min-w-screen">
       <div className="bg-blue-300 min-w-[1140] min-h-screen">
-        <Button>Click me</Button>
       </div>
     </div>
   );
