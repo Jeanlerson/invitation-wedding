4ecf159145071dcf25e92d4325e85bbcc96d5263
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2025-09-18 15:47:28 -0300

    testando-cores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { Carousel } from "@/components/ui/carousel";
 
 export default function Page() {
   return (
-    <div className="flex justify-center min-h-screen min-w-screen">
-      <div className="bg-blue-300 min-w-[1140] min-h-screen">
+    <div className="flex justify-center min-h-screen min-w-screen bg-[#003366]">
+      <div className="bg-[#000D33] min-w-[1140] min-h-screen">
+        <h1 className="text-8xl text-[#5CADFF]">Convite de Casamento</h1>
       </div>
     </div>
   );
