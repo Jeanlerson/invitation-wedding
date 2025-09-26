ee3928dfcbe4df8ce26d97372ec5bcb23a7b4581
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2025-09-26 15:04:35 -0300

    fontes-importadas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,9 @@ const greatVibes = Great_Vibes({
 export default function Page() {
   return (
     <div className="flex justify-center min-h-screen min-w-screen bg-[#003366]">
-      <div className="bg-[#000D33] min-w-[1140] min-h-screen">
-        <h1 className="text-8xl text-[#5CADFF]">Convite de Casamento</h1>
+      <div className="bg-[#000D33] min-w-[1140] min-h-screen p-5">
+        <h1 className="font-great-vibes text-8xl text-[#5CADFF]">Convite de Casamento</h1>
+        <p className="font-poppins text-2xl text-white">Convidamos você para celebrar conosco um dos dias mais especiais de nossas vidas!</p>
       </div>
     </div>
   );
