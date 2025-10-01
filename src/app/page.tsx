350ae5a139f0726d70e3739b2a9e11a68a673666
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2025-10-01 17:01:47 -0300

    add-botoes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,10 @@ export default function Page() {
       <div className="bg-[#000D33] min-w-[1140] min-h-screen p-5">
         <h1 className="font-great-vibes text-8xl text-[#5CADFF]">Convite de Casamento</h1>
         <p className="font-poppins text-2xl text-white">Convidamos você para celebrar conosco um dos dias mais especiais de nossas vidas!</p>
-        <Button className="mt-4 bg-[#0059B3] text-white hover:bg-[#004C99]">Confirmar Convite</Button>
+        <Button className="mt-4 m-1 bg-[#0059B3] text-white hover:bg-[#004C99]">Confirmar Convite</Button>
+        <Button className="mt-4 m-1 bg-[#0059B3] text-white hover:bg-[#004C99]">Selecionar presente</Button>
         <Input className="mt-10 bg-white text-black" placeholder="Digite seu nome para confirmar presença" />
+        <Input className="mt-10 bg-white text-black" placeholder="Digite a quantidade de acompanhantes" />
         <Card className="mt-10 p-5 bg-[#0059B3] text-white">
           <h2 className="text-3xl mb-2">Detalhes do Evento</h2>
           <p>Data: 15 de Dezembro de 2024</p>
