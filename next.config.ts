8e2e70e5fbc6f5e8b5f0a0a2de667365e2deeb3b
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2025-10-24 14:16:16 -0300

    finalizado-integracao-presentes

--- next.config.ts
+++ next.config.ts
@@ -18,33 +18,64 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "casafreitas2.vteximg.com.br", // ✅ adicione este
+        hostname: "casafreitas2.vteximg.com.br",
       },
       {
         protocol: "https",
-        hostname: "images.tcdn.com.br", // ✅ adicione este
+        hostname: "images.tcdn.com.br",
       },
       {
         protocol: "https",
-        hostname: "down-br.img.susercontent.com", // ✅ adicione este
+        hostname: "down-br.img.susercontent.com",
       },
       {
         protocol: "https",
-        hostname: "cdn.awsli.com.br", // ✅ adicione este
+        hostname: "cdn.awsli.com.br",
       },
       {
         protocol: "https",
-        hostname: "http2.mlstatic.com", // ✅ adicione este
+        hostname: "http2.mlstatic.com",
       },
       {
         protocol: "https",
-        hostname: "www.havan.com.br", // ✅ adicione este
+        hostname: "www.havan.com.br",
       },
       {
         protocol: "https",
-        hostname: "imgs.extra.com.br", // ✅ adicione este
+        hostname: "imgs.extra.com.br",
       },
-      
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.vnda.com.br",
+      },
+      {
+        protocol: "https",
+        hostname: "fernet.vteximg.com.br",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.leroymerlin.com.br",
+      },
+      {
+        protocol: "https",
+        hostname: "imgs.casasbahia.com.br",
+      },
+      {
+        protocol: "https",
+        hostname: "reidacutelaria.vtexassets.com",
+      },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn3.gstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn1.gstatic.com",
+      },
     ],
   },
 };
