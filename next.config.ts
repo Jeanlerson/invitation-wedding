97736cd1d0d8d3c63ec8ccb8f7dc3178f4eebffb
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2025-10-24 10:50:17 -0300

    correcao-img-firebase

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,35 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "lh3.googleusercontent.com", // útil se tiver imagens do Google
       },
+      {
+        protocol: "https",
+        hostname: "casafreitas2.vteximg.com.br", // ✅ adicione este
+      },
+      {
+        protocol: "https",
+        hostname: "images.tcdn.com.br", // ✅ adicione este
+      },
+      {
+        protocol: "https",
+        hostname: "down-br.img.susercontent.com", // ✅ adicione este
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.awsli.com.br", // ✅ adicione este
+      },
+      {
+        protocol: "https",
+        hostname: "http2.mlstatic.com", // ✅ adicione este
+      },
+      {
+        protocol: "https",
+        hostname: "www.havan.com.br", // ✅ adicione este
+      },
+      {
+        protocol: "https",
+        hostname: "imgs.extra.com.br", // ✅ adicione este
+      },
+      
     ],
   },
 };
