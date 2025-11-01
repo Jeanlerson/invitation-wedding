2c556657eb6678b2888dbe847e9104d7e5d62fec
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2025-11-01 08:47:07 -0300

    atl-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Page() {
     
     /*
     <div className="flex justify-center min-h-screen min-w-screen bg-[#003366]">
-      <div className="bg-[#000D33] min-w-[1140] min-h-screen p-5">
+      <div className="bg-[z] min-w-[1140] min-h-screen p-5">
         <h1 className="font-great-vibes text-8xl text-[#5CADFF]">Convite de Casamento</h1>
         <p className="font-poppins text-2xl text-white">Convidamos você para celebrar conosco um dos dias mais especiais de nossas vidas!</p>
         <Button className="mt-4 m-1 bg-[#0059B3] text-white hover:bg-[#004C99]">Confirmar Convite</Button>
